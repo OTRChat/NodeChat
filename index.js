2dc9ffa6d3cb9d0887032d56b10b2da0a13e48ab
Josh Ghent <me@joshghent.com>
2016-06-21 23:04:53 +0100

    Basic working prototype.

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ var app = express();
 var http = require('http').Server(app);
 var port = 9876;
 var path = require('path');
+var io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('A user has connected: ' + socket.id);
+
+  socket.on('chat message', function(message){
+    io.emit('chat message', message)
+  });
+
+  socket.on('disconnect', function(){
+    console.log(socket.name + ' has disconnected.' + socket.id);
+  });
+
+
 });
 
 http.listen(port, function(){
