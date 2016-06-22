b205fea6e2b90b1ffe31a84e6e468e654ab1f734
Josh Ghent <me@joshghent.com>
2016-06-22 19:23:23 +0100

    Added notification sound. Changed the server emit to send to all apart from the sender.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ io.on('connection', function(socket){
   console.log('A user has connected: ' + socket.id);
 
   socket.on('chat message', function(message){
-    io.emit('chat message', message)
+    socket.broadcast.emit('chat message', message)
   });
 
   socket.on('disconnect', function(){
